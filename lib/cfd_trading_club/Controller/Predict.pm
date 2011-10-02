25f4ffece0d9eb1de6945412828b9ceff4c11fa9
Zane Moser <zjmoser@gmail.com>
2011-10-02 19:11:02 +1300

    Made countdown correctly display time to 12:00

--- lib/cfd_trading_club/Controller/Predict.pm
+++ lib/cfd_trading_club/Controller/Predict.pm
@@ -34,19 +34,40 @@ sub index :Path :Args(0) {
     push @predictors, \%data;
     my @confidence_levels = ('Good guess', 'Ballsy', 'Sensei');
 
-    # Determine current time
-    my $dt = DateTime->now;
-    my %time_remaining = (
-        hours => 0,
-        mins  => 1,
-        secs  => 10,
-    );
-
     $c->stash(
         predictors        => \@predictors,
         confidence_levels => \@confidence_levels,
-        time_remaining    => \%time_remaining,
     );
+    $c->forward('stash_time_to_close');
+}
+
+=head2 calculate_time_to_close
+
+Calculates time duration until the next 12:00 (am or pm, whichever is closest).
+Returns a hashref
+{
+    hours =>
+    mins  =>
+    secs  =>
+}
+
+=cut
+
+sub stash_time_to_close :Private {
+    my ( $self, $c ) = @_;
+
+    my $dt = DateTime->now;
+    $dt->set_time_zone('Pacific/Auckland');
+
+    my $hours_remaning = 11 - $dt->hour_12_0;
+    my $mins_remaning  = 59 - $dt->min;
+    my $secs_remaning  = 59 - $dt->sec;
+
+    $c->stash->{time_remaining} = {
+        hours => $hours_remaning,
+        mins  => $mins_remaning,
+        secs  => $secs_remaning,
+    };
 }
 
 =head1 AUTHOR
